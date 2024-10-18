c250fac3493912e1d34f078172253f503af8377d
Wen Liang <346225483@qq.com>
2024-10-18 19:36:43 +1300

    Optimize admin manage display

--- js/config.js
+++ js/config.js
@@ -1,4 +1,8 @@
-// Base Host
-const API_BASE_URL = 'http://localhost:5000';
+// Base Host Use nginx
+// const API_BASE_URL = 'http://localhost:8080/api';
+
+// Base Host Python service
+const API_BASE_URL = 'http://localhost:2000';
+
 
 export { API_BASE_URL };
